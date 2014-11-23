006972187c2db7c254f7d076158757a772d8693b
Abhishek Mukhopadhyay <amukhop1@server.fake>
2014-11-22 17:47:08 -0800

    Constants file trimmed as well

--- frontend/app/util/Constants.java
+++ frontend/app/util/Constants.java
@@ -3,41 +3,6 @@ package util;
 public class Constants {
 
   public static final String HOSTNAME = "http://localhost:9010/";
-
-  // Publication
-  public static final String GET_PUBLICATION_ROUTE = "getPublication";
-  public static final String ALL_PUBLICATIONS_ROUTE = "getAllPublications";
-  public static final String DELETE_PUBLICATION_ROUTE = "deletePublication";
-
-  // JournalPublication
-  public static final String GET_JOURNAL_PUBLICATION_ROUTE = "getJournalPublication";
-  public static final String ALL_JOURNAL_PUBLICATIONS_ROUTE = "getAllJournalPublications";
-  public static final String ADD_JOURNAL_PUBLICATION_ROUTE = "addJournalPublication";
-  public static final String UPDATE_JOURNAL_PUBLICATION_ROUTE = "updateJournalPublication";
-
-  // BookPublication
-  public static final String GET_BOOK_PUBLICATION_ROUTE = "getBookPublication";
-  public static final String ALL_BOOK_PUBLICATIONS_ROUTE = "getAllBookPublications";
-  public static final String ADD_BOOK_PUBLICATION_ROUTE = "addBookPublication";
-  public static final String UPDATE_BOOK_PUBLICATION_ROUTE = "updateBookPublication";
-
-  // BookChapterPublication
-  public static final String GET_BOOKCHAPTER_PUBLICATION_ROUTE = "getBookChapterPublication";
-  public static final String ALL_BOOKCHAPTER_PUBLICATIONS_ROUTE = "getAllBookChapterPublications";
-  public static final String ADD_BOOKCHAPTER_PUBLICATION_ROUTE = "addBookChapterPublication";
-  public static final String UPDATE_BOOKCHAPTER_PUBLICATION_ROUTE = "updateBookChapterPublication";
-
-  // ConferencePublication
-  public static final String GET_CONFERENCE_PUBLICATION_ROUTE = "getConferencePublication";
-  public static final String ALL_CONFERENCE_PUBLICATIONS_ROUTE = "getAllConferencePublications";
-  public static final String ADD_CONFERENCE_PUBLICATION_ROUTE = "addConferencePublication";
-  public static final String UPDATE_CONFERENCE_PUBLICATION_ROUTE = "updateConferencePublication";  
-
-  // WebPagePublication
-  public static final String GET_WEBPAGE_PUBLICATION_ROUTE = "getWebPagePublication";
-  public static final String ALL_WEBPAGE_PUBLICATIONS_ROUTE = "getAllWebPagePublications";
-  public static final String ADD_WEBPAGE_PUBLICATION_ROUTE = "addWebPagePublication";
-  public static final String UPDATE_WEBPAGE_PUBLICATION_ROUTE = "updateWebPagePublication";
   
   //Application
   public static final String GET_COAUTHOR_GRAPH_DATA = "getCoAuthorGraphData";
