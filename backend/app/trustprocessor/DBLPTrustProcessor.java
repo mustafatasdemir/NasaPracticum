9e62b9bf4fd906aaa326e2f6301a939942ca2651
jishavm <jisha.muthiyil@sv.cmu.edu>
2014-11-22 18:20:12 -0800

    modified

--- backend/app/trustprocessor/DBLPTrustProcessor.java
+++ backend/app/trustprocessor/DBLPTrustProcessor.java
@@ -205,7 +205,8 @@ public class DBLPTrustProcessor {
 		return dblpTrustModel;
 	}
 
-	/*private KCoauthorship calculateKCoauthorship(int authorId) throws Exception {
+	/*Commented by Jisha
+	 * private KCoauthorship calculateKCoauthorship(int authorId) throws Exception {
 		double coauthorshipCount = 0;
 
 		TimeScale timeScale = new TimeScale();
