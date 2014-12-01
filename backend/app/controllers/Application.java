8993b3934814047d582f04806849542cae0c47e6
Mustafa Tasdemir <mustafa.tasdemir@hotmail.com.tr>
2014-12-01 12:37:46 -0800

    db password changed for prod

--- backend/app/controllers/Application.java
+++ backend/app/controllers/Application.java
@@ -7,14 +7,6 @@ import views.html.*;
 public class Application extends Controller {
 
   public static Result index() {
-	  
-	try{
-		DBLPTrustProcessor dblp =  new DBLPTrustProcessor();
-		 dblp.testIt();
-	}catch(Exception e){
-		 e.printStackTrace();;
-	}
-	
     return ok(index.render("Your new application is ready."));
   }
 
