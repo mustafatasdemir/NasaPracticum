1e5a8cb09208adfbc92c383078fa7e977fece3fc
Abhishek Mukhopadhyay <amukhop1@server.fake>
2014-12-03 19:15:59 -0800

    Backend use cases done uptil controller. Got placeholder code in model. Waiting to sync one by one with stored procedures

--- backend/app/controllers/GraphReturnObject.java
+++ backend/app/controllers/GraphReturnObject.java
@@ -15,7 +15,7 @@ public class GraphReturnObject extends Controller{
 		// TODO Auto-generated constructor stub
 	}
 	
-	// Controller to receive request for data required to render Co-Author graph. Receives the topic as a 
+	// Controller to receive request for data required to render Co-Author graph by topic. Receives the topic as a 
 	// String parameter encoded as a URL in UTF-8. Calls on the model by the same name to contact the database 
 	// for data and returns the model as a Json object
 	public static Result getCoAuthorGraphDataByTopic(String parameter) throws SQLException, UnsupportedEncodingException
@@ -43,6 +43,74 @@ public class GraphReturnObject extends Controller{
 	    }
 
 	    return ok(Json.toJson(graphReturnObject));
+	}
+	
+	// Controller to receive request for data required to render Author Publication graph by topic. Receives the topic as a 
+	// String parameter encoded as a URL in UTF-8. Calls on the model by the same name to contact the database 
+	// for data and returns the model as a Json object
+	public static Result getAuthorPublicationGraphDataByTopic(String parameter) throws SQLException, UnsupportedEncodingException
+	{
+		parameter = URLDecoder.decode(parameter, "UTF-8");
+		models.GraphReturnObject graphReturnObject = new models.GraphReturnObject();
+
+		graphReturnObject.AuthorPublicationGraphDataByTopic(parameter);
+
+		if (graphReturnObject.nodes == null) {
+			return notFound("No data found");
+		}
+
+		return ok(Json.toJson(graphReturnObject));
+	}
+	
+	// Controller to receive request for data required to render Co-Publication graph. Receives the topic as a 
+	// String parameter encoded as a URL in UTF-8. Calls on the model by the same name to contact the database 
+	// for data and returns the model as a Json object
+	public static Result getCoPublicationGraphDataByTopic(String parameter) throws SQLException, UnsupportedEncodingException
+	{
+		parameter = URLDecoder.decode(parameter, "UTF-8");
+		models.GraphReturnObject graphReturnObject = new models.GraphReturnObject();
+
+		graphReturnObject.CoPublicationGraphDataByTopic(parameter);
+
+		if (graphReturnObject.nodes == null) {
+			return notFound("No data found");
+		}
+
+		return ok(Json.toJson(graphReturnObject));
+	}
+	
+	// Controller to receive request for data required to render Co-Author graph by author. Receives the author name as a 
+	// String parameter encoded as a URL in UTF-8. Calls on the model by the same name to contact the database 
+	// for data and returns the model as a Json object
+	public static Result getCoAuthorGraphDataByAuthor(String parameter) throws SQLException, UnsupportedEncodingException
+	{
+		parameter = URLDecoder.decode(parameter, "UTF-8");
+		models.GraphReturnObject graphReturnObject = new models.GraphReturnObject();
+
+		graphReturnObject.CoAuthorGraphDataByAuthor(parameter);
+
+		if (graphReturnObject.nodes == null) {
+			return notFound("No data found");
+		}
+
+		return ok(Json.toJson(graphReturnObject));
+	}
+
+	// Controller to receive request for data required to render Author Publication graph by author. Receives the author name as a 
+	// String parameter encoded as a URL in UTF-8. Calls on the model by the same name to contact the database 
+	// for data and returns the model as a Json object
+	public static Result getAuthorPublicationGraphDataByAuthor(String parameter) throws SQLException, UnsupportedEncodingException
+	{
+		parameter = URLDecoder.decode(parameter, "UTF-8");
+		models.GraphReturnObject graphReturnObject = new models.GraphReturnObject();
+
+		graphReturnObject.AuthorPublicationGraphDataByAuthor(parameter);
+
+		if (graphReturnObject.nodes == null) {
+			return notFound("No data found");
+		}
+
+		return ok(Json.toJson(graphReturnObject));
 	}
 
 }
--- backend/app/models/GraphReturnObject.java
+++ backend/app/models/GraphReturnObject.java
@@ -120,4 +120,347 @@ public class GraphReturnObject {
 			links.add(new Link(link[0], link[1], coAuthorLinks.get(key)));
 		}
 	}
+
+	public void AuthorPublicationGraphDataByTopic(String parameter) throws SQLException
+	{
+		Connection connection = DB.getConnection();
+		PreparedStatement preparedStatement = null;
+		String[] topics = null;
+		
+		String[] parameters = parameter.split("&");
+		
+		if(parameters[0].contains(","))
+		{
+			topics = parameters[0].split(",");
+			preparedStatement = util.SQLQueries.getCoAuthorshipMultipleTopicsNodeInfo(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipNodeInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}		
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while(resultSet.next())
+		{
+			if(nodes==null)
+			{
+				nodes = new ArrayList<Node>();
+			}
+			Node node = new Node("", topics == null ? parameters[0] : topics[resultSet.getInt("Topic")], resultSet.getString("AuthorName"), resultSet.getString("PublicationList"), resultSet.getString("authorId"), "Author", resultSet.getLong(topics == null ? "PublicationCount" : "citationCount"));
+			nodes.add(node);
+		}
+		
+		if(topics != null)
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfoMultipleTopic(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		
+		HashMap<String, Long> coAuthorLinks = new HashMap<String, Long>();
+		String edge = "";
+		String mirrorEdge = "";
+		while(resultSet.next())
+		{
+			if(links == null)
+			{
+				links = new ArrayList<Link>();
+			}
+			String[] authors = resultSet.getString("Authors").split(",");
+			for(int i = 0; i < authors.length - 1; i++)
+			{
+				for(int j = i + 1; j < authors.length; j++)
+				{
+					edge = authors[i].trim() + "," + authors[j].trim();
+					mirrorEdge = authors[j].trim() + "," + authors[i].trim();
+					if(coAuthorLinks.containsKey(edge))
+					{
+						coAuthorLinks.put(edge, coAuthorLinks.get(edge).longValue() + 1);
+					}
+					else if (coAuthorLinks.containsKey(mirrorEdge))
+					{
+						coAuthorLinks.put(mirrorEdge, coAuthorLinks.get(mirrorEdge).longValue() + 1);
+					}
+					else
+					{
+						coAuthorLinks.put(edge.toString(), 1L);
+					}
+					edge = "";
+					mirrorEdge = "";
+				}
+			}
+		}
+		
+		preparedStatement.close();
+		
+		for(String key: coAuthorLinks.keySet())
+		{
+			String[] link = key.split(",");
+			links.add(new Link(link[0], link[1], coAuthorLinks.get(key)));
+		}		
+	}
+
+	public void CoPublicationGraphDataByTopic(String parameter) throws SQLException
+	{
+		Connection connection = DB.getConnection();
+		PreparedStatement preparedStatement = null;
+		String[] topics = null;
+		
+		String[] parameters = parameter.split("&");
+		
+		if(parameters[0].contains(","))
+		{
+			topics = parameters[0].split(",");
+			preparedStatement = util.SQLQueries.getCoAuthorshipMultipleTopicsNodeInfo(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipNodeInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}		
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while(resultSet.next())
+		{
+			if(nodes==null)
+			{
+				nodes = new ArrayList<Node>();
+			}
+			Node node = new Node("", topics == null ? parameters[0] : topics[resultSet.getInt("Topic")], resultSet.getString("AuthorName"), resultSet.getString("PublicationList"), resultSet.getString("authorId"), "Author", resultSet.getLong(topics == null ? "PublicationCount" : "citationCount"));
+			nodes.add(node);
+		}
+		
+		if(topics != null)
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfoMultipleTopic(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		
+		HashMap<String, Long> coAuthorLinks = new HashMap<String, Long>();
+		String edge = "";
+		String mirrorEdge = "";
+		while(resultSet.next())
+		{
+			if(links == null)
+			{
+				links = new ArrayList<Link>();
+			}
+			String[] authors = resultSet.getString("Authors").split(",");
+			for(int i = 0; i < authors.length - 1; i++)
+			{
+				for(int j = i + 1; j < authors.length; j++)
+				{
+					edge = authors[i].trim() + "," + authors[j].trim();
+					mirrorEdge = authors[j].trim() + "," + authors[i].trim();
+					if(coAuthorLinks.containsKey(edge))
+					{
+						coAuthorLinks.put(edge, coAuthorLinks.get(edge).longValue() + 1);
+					}
+					else if (coAuthorLinks.containsKey(mirrorEdge))
+					{
+						coAuthorLinks.put(mirrorEdge, coAuthorLinks.get(mirrorEdge).longValue() + 1);
+					}
+					else
+					{
+						coAuthorLinks.put(edge.toString(), 1L);
+					}
+					edge = "";
+					mirrorEdge = "";
+				}
+			}
+		}
+		
+		preparedStatement.close();
+		
+		for(String key: coAuthorLinks.keySet())
+		{
+			String[] link = key.split(",");
+			links.add(new Link(link[0], link[1], coAuthorLinks.get(key)));
+		}
+		
+	}
+
+	public void CoAuthorGraphDataByAuthor(String parameter) throws SQLException
+	{
+		Connection connection = DB.getConnection();
+		PreparedStatement preparedStatement = null;
+		String[] topics = null;
+		
+		String[] parameters = parameter.split("&");
+		
+		if(parameters[0].contains(","))
+		{
+			topics = parameters[0].split(",");
+			preparedStatement = util.SQLQueries.getCoAuthorshipMultipleTopicsNodeInfo(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipNodeInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}		
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while(resultSet.next())
+		{
+			if(nodes==null)
+			{
+				nodes = new ArrayList<Node>();
+			}
+			Node node = new Node("", topics == null ? parameters[0] : topics[resultSet.getInt("Topic")], resultSet.getString("AuthorName"), resultSet.getString("PublicationList"), resultSet.getString("authorId"), "Author", resultSet.getLong(topics == null ? "PublicationCount" : "citationCount"));
+			nodes.add(node);
+		}
+		
+		if(topics != null)
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfoMultipleTopic(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		
+		HashMap<String, Long> coAuthorLinks = new HashMap<String, Long>();
+		String edge = "";
+		String mirrorEdge = "";
+		while(resultSet.next())
+		{
+			if(links == null)
+			{
+				links = new ArrayList<Link>();
+			}
+			String[] authors = resultSet.getString("Authors").split(",");
+			for(int i = 0; i < authors.length - 1; i++)
+			{
+				for(int j = i + 1; j < authors.length; j++)
+				{
+					edge = authors[i].trim() + "," + authors[j].trim();
+					mirrorEdge = authors[j].trim() + "," + authors[i].trim();
+					if(coAuthorLinks.containsKey(edge))
+					{
+						coAuthorLinks.put(edge, coAuthorLinks.get(edge).longValue() + 1);
+					}
+					else if (coAuthorLinks.containsKey(mirrorEdge))
+					{
+						coAuthorLinks.put(mirrorEdge, coAuthorLinks.get(mirrorEdge).longValue() + 1);
+					}
+					else
+					{
+						coAuthorLinks.put(edge.toString(), 1L);
+					}
+					edge = "";
+					mirrorEdge = "";
+				}
+			}
+		}
+		
+		preparedStatement.close();
+		
+		for(String key: coAuthorLinks.keySet())
+		{
+			String[] link = key.split(",");
+			links.add(new Link(link[0], link[1], coAuthorLinks.get(key)));
+		}
+		
+	}
+
+	public void AuthorPublicationGraphDataByAuthor(String parameter) throws SQLException
+	{
+		Connection connection = DB.getConnection();
+		PreparedStatement preparedStatement = null;
+		String[] topics = null;
+		
+		String[] parameters = parameter.split("&");
+		
+		if(parameters[0].contains(","))
+		{
+			topics = parameters[0].split(",");
+			preparedStatement = util.SQLQueries.getCoAuthorshipMultipleTopicsNodeInfo(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipNodeInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}		
+		
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		while(resultSet.next())
+		{
+			if(nodes==null)
+			{
+				nodes = new ArrayList<Node>();
+			}
+			Node node = new Node("", topics == null ? parameters[0] : topics[resultSet.getInt("Topic")], resultSet.getString("AuthorName"), resultSet.getString("PublicationList"), resultSet.getString("authorId"), "Author", resultSet.getLong(topics == null ? "PublicationCount" : "citationCount"));
+			nodes.add(node);
+		}
+		
+		if(topics != null)
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfoMultipleTopic(connection, topics, parameters[1], Integer.parseInt(parameters[2])/topics.length);
+		}
+		else
+		{
+			preparedStatement = util.SQLQueries.getCoAuthorshipLinkInfo(connection, (parameters[0].matches("All") ? "" : parameters[0]), parameters[1], Integer.parseInt(parameters[2]));
+		}
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		
+		HashMap<String, Long> coAuthorLinks = new HashMap<String, Long>();
+		String edge = "";
+		String mirrorEdge = "";
+		while(resultSet.next())
+		{
+			if(links == null)
+			{
+				links = new ArrayList<Link>();
+			}
+			String[] authors = resultSet.getString("Authors").split(",");
+			for(int i = 0; i < authors.length - 1; i++)
+			{
+				for(int j = i + 1; j < authors.length; j++)
+				{
+					edge = authors[i].trim() + "," + authors[j].trim();
+					mirrorEdge = authors[j].trim() + "," + authors[i].trim();
+					if(coAuthorLinks.containsKey(edge))
+					{
+						coAuthorLinks.put(edge, coAuthorLinks.get(edge).longValue() + 1);
+					}
+					else if (coAuthorLinks.containsKey(mirrorEdge))
+					{
+						coAuthorLinks.put(mirrorEdge, coAuthorLinks.get(mirrorEdge).longValue() + 1);
+					}
+					else
+					{
+						coAuthorLinks.put(edge.toString(), 1L);
+					}
+					edge = "";
+					mirrorEdge = "";
+				}
+			}
+		}
+		
+		preparedStatement.close();
+		
+		for(String key: coAuthorLinks.keySet())
+		{
+			String[] link = key.split(",");
+			links.add(new Link(link[0], link[1], coAuthorLinks.get(key)));
+		}
+		
+	}
 }
